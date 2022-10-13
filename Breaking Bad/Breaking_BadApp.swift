6d64fb7a23a973cd8ca16f0500d9d2fbb8cb2abd
Relisiuol <49282232+relisiuol@users.noreply.github.com>
2022-10-13 16:45:20 +0200

    Initial Commit

---
+++ Breaking Bad/Breaking_BadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaking_BadApp.swift
+//  Breaking Bad
+//
+//  Created by Louis Vedel on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Breaking_BadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
